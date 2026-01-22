b67a4f36c9383d264ff5fe8d2a0947ec5778da4f
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-01-22 18:54:18 +0100

    feat: add HUD state management using Zustand

---
+++ client/src/stores/HudStore.tsx
@@ -0,0 +1,16 @@
+import { create } from 'zustand';
+import { type GSIPayload } from '../types/gsi';
+
+interface HUDState {
+    data: GSIPayload | null;
+    isConnected: boolean;
+    updateState: (newData: GSIPayload) => void;
+    setConnected: (status: boolean) => void;
+}
+
+export const useHUDStore = create<HUDState>((set) => ({
+    data: null,
+    isConnected: false,
+    setConnected: (status) => set({ isConnected: status }),
+    updateState: (newData) => set({ data: newData }),
+}));
