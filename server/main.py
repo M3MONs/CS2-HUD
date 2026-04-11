4b2536991309d8f49cf69098350d10cb638d024d
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-04-11 11:58:19 +0200

    feat: add CORS middleware configuration in main.py

--- server/main.py
+++ server/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.gsi import router as gsi_router
 from api.websocket import router as ws_router
+from api.config import router as config_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(gsi_router)
 app.include_router(ws_router)
+app.include_router(config_router)
