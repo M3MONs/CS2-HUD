5e0cf0e0bd45e9240dd8aebecc69e37600db108c
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-04-11 12:14:18 +0200

    feat: add server proxy configuration for API and WebSocket in vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,23 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react'
 
+const BACKEND_URL = 'http://localhost:8000';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    proxy: {
+      '/api': {
+        target: BACKEND_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/ws': {
+        target: BACKEND_URL,
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
 })
