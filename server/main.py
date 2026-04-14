cd11d47bc2dd5b655e033dbaa43471af8cc00190
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-04-14 18:00:41 +0200

    feat: implement lifespan context manager for FastAPI to manage background tasks

--- server/main.py
+++ server/main.py
@@ -1,10 +1,19 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api.gsi import router as gsi_router
+from api.gsi import router as gsi_router, _tasks as gsi_tasks
 from api.websocket import router as ws_router
 from api.config import router as config_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    for task in list(gsi_tasks):
+        task.cancel()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
