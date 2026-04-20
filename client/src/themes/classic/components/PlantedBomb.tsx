215c84427fc39f730b2d1bd52180d50f97c4cb4b
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-04-20 15:01:47 +0200

    refactor: simplify PlantedBomb component by removing props and optimizing structure

--- client/src/themes/classic/components/PlantedBomb.tsx
+++ client/src/themes/classic/components/PlantedBomb.tsx
@@ -1,13 +1,10 @@
-import type { PlantedBombProps } from "../types";
+import { memo } from "react";
 
-export const PlantedBomb = ({ x, y, vw, vh }: PlantedBombProps) => {
-    const px = (x / 100) * vw - 5;
-    const py = (y / 100) * vh - 5;
+export const PlantedBomb = memo(function PlantedBomb() {
     return (
         <div
             className="classic-radar__planted-bomb"
-            style={{ transform: `translate(${px}px, ${py}px) rotate(45deg)` }}
             title="Bomb"
         />
     );
-};
+});
