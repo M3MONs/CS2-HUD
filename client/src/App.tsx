e83b04eb9c7f6f8ca335e9aeefa1734a7568ce53
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-07-28 12:20:54 +0200

    feat: add LayoutPage route to App for layout editing access

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router";
 import HUDPage from "./pages/HUD/HUDPage";
+import LayoutPage from "./pages/Layout/LayoutPage";
 import SettingsPage from "./pages/Settings/SettingsPage";
 import HomePage from "./pages/Home/HomePage";
 
@@ -8,6 +9,7 @@ const App = () => {
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/hud" element={<HUDPage />} />
+            <Route path="/layout" element={<LayoutPage />} />
             <Route path="/settings" element={<SettingsPage />} />
             <Route path="*" element={<HomePage />} />
         </Routes>
